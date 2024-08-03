466a7ccf66bef2deab4d15800d605de3273259ea
Mustafa Süleyman Kınık <suleymankinik@outlook.com>
2024-08-03 22:44:23 +0300

    Add the new examples code in AppModule

--- app/src/main/java/com/example/di/AppModule.kt
+++ app/src/main/java/com/example/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.example.di
 
+import android.content.Context
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import java.text.SimpleDateFormat
+import java.util.Locale
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -23,5 +28,18 @@ class AppModule {
     @Named("BString")
     fun provideBString() = "B String"
 
+    @Provides
+    @Singleton
+    fun provideSimpleDateFormat(): SimpleDateFormat =  SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.US)
+
+    @Provides
+    @Singleton
+    fun provideCircularProgressDrawable(@ApplicationContext context: Context): CircularProgressDrawable {
+        return CircularProgressDrawable(context).apply {
+            strokeWidth = 5f
+            centerRadius = 30f
+            start()
+        }
+    }
 
 }
